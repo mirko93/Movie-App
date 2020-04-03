5759b67bde904886171194ee9e089e576021efa7
mirko93 <skopljak.mirko93@gmail.com>
2020-04-03 11:58:28 +0200

    laravel-livewire

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\project\movie-app\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\project\movie-app\resources\css\main.css */"./resources/css/main.css");
+__webpack_require__(/*! E:\VueJS project\Movie-App\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\VueJS project\Movie-App\resources\css\main.css */"./resources/css/main.css");
 
 
 /***/ })
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,5 @@
 <div class="relative mt-3 md:mt-0">
-    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 py-2 pl-8 focus:outline-none focus:shadow-outline" placeholder="Search...">
+    <input wire:mode.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 py-2 pl-8 focus:outline-none focus:shadow-outline" placeholder="Search...">
     <div class="absolute top-0">
         <svg class="fill-current text-gray-500 w-4 mt-3 ml-2" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-1 0 136 136.21852">
             <g id="surface1">
@@ -8,9 +8,33 @@
         </svg>
     </div>
 
+    <div wire:loading class="spinner top-0 right-0 mr-4 mt-5"></div>
+
+    @if (strlen($search) >= 2)
     <div class="absolute bg-gray-800 rounded w-64 mt-4">
-        <ul>
-            <li class="border-b border-gray-700"></li>
-        </ul>
-    </div>
+        @if ($searchResults->count() > 0)
+            <ul>
+                @foreach ($searchResults as $result)
+                    <li class="border-b border-gray-700">
+                        <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+                            @if ($loop->last) @keydown.tab="isOpen = false" @endif
+                        >
+                            @if ($result['poster_path'])
+                            <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
+                            @else
+                            <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                            @endif
+                            
+                            <span class="ml-4">{{ $result['title'] }}</span>
+                        </a>
+                    </li>
+                @endforeach
+
+            </ul>
+        @else
+            <div class="px-3 py-3">No results for "{{ $search }}"</div>
+        @endif
+
+    </div>          
+    @endif
 </div>
