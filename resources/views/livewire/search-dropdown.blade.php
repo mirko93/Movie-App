56a98c3e881aeb21887e43213bf66593e9858103
mirko93 <skopljak.mirko93@gmail.com>
2020-04-03 12:27:35 +0200

    "laravel-livewire edit"

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -11,14 +11,13 @@
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-5"></div>
 
     @if (strlen($search) >= 2)
-    <div class="absolute bg-gray-800 rounded w-64 mt-4">
+    <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
         @if ($searchResults->count() > 0)
             <ul>
                 @foreach ($searchResults as $result)
                     <li class="border-b border-gray-700">
-                        <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
-                            @if ($loop->last) @keydown.tab="isOpen = false" @endif
-                        >
+                        <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+
                             @if ($result['poster_path'])
                             <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
                             @else
