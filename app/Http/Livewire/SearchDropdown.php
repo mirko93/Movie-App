ca530a90456570cfa5950b8fca17b528bd454886
mirko93 <skopljak.mirko93@gmail.com>
2020-04-03 13:27:15 +0200

    "laravel-livewire - edit"

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -8,19 +8,23 @@ use Livewire\Component;
 class SearchDropdown extends Component
 {
     public $search = '';
+    // problem kad je prazan string u input search nece da izbaci rezultat
+    // iako je definisan u search-dropdown.blade.php
 
     public function render()
     {
-        $searchResults = [];
+        $searchMovie = [];
 
         if (strlen($this->search) >= 2) {
-            $searchResults = Http::withToken(config('services.tmdb.token'))
-                ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
+            $searchMovie = Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/search/movie?query=' . $this->search)
                 ->json()['results'];
         }
 
+        // dd($searchMovie);
+
         return view('livewire.search-dropdown', [
-            'searchResults' => collect($searchResults)->take(7),
+            'searchMovie' => collect($searchMovie)->take(7),
         ]);
     }
 }
