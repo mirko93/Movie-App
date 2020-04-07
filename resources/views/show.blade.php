fd95ba05ca698b85a06d9c4683d57f145e1477aa
mirko93 <skopljak.mirko93@gmail.com>
2020-04-07 17:16:52 +0200

    refactoring to view models

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -31,6 +31,10 @@
                                 <div>{{ $crew['name'] }}</div>
                                 <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
                             </div>
+                            @else
+
+                            @break
+
                             @endif
                         @endforeach
                         
@@ -85,6 +89,10 @@
                                 </div>
                             </div>
                         </div>
+                    @else
+
+                    @break
+
                     @endif
                 @endforeach
             </div>
@@ -105,6 +113,10 @@
                                 <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                         </div>
+                    @else
+
+                    @break
+
                     @endif
                 @endforeach
             </div>
